a0f7a2afae13761c6672f0d9c77b56583ddc13fd
sumin <sue4869@naver.com>
2022-03-12 17:12:24 +0900

    회원관리예제 - 홈화면, 등록, 조회

---
+++ src/main/java/hellostart/startspring/controller/HomeController.java
@@ -0,0 +1,13 @@
+package hellostart.startspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String hone() {
+        return "home";
+    }
+}
--- src/main/java/hellostart/startspring/controller/MemberController.java
+++ src/main/java/hellostart/startspring/controller/MemberController.java
@@ -1,34 +1,45 @@
 package hellostart.startspring.controller;
 
+import hellostart.startspring.domain.Member;
 import hellostart.startspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 
 @Controller
 public class MemberController {
 
-    //DI 주입 방법 1: 생성자 주입.생성자에 의에 memberService가 memberController에 주입된다.
-    // 의존관계가 실행중에 동적으로 변하는 경우(런타임에 중간에 바뀌는것)가 없기 때문에 권장
-    private final MemberService memberService;
+   private final MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-    //DI 주입 방법 2:필드주입 -> 단점: 중간에 바꿔치기 할 방법이 없으므로 잘 안쓰는 방법
-/*
-    @Autowired
-    private MemberService memberService;
-*/
 
-    //DI 주입 방법 3:setter주입 -> 단점: set생성자 public으로 열려있어야한다.아무나 접근가능
-/*
-    private MemberService memberService;
-
-    @Autowired
-    public void setMemberService(MemberService memberService) {
-        this.memberService = memberService;
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
     }
- */
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMember();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
 }
