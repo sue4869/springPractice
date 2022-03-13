fd996947870679e6bd6e7934a0675c4a3ef73ea6
sumin <sue4869@naver.com>
2022-03-13 15:26:55 +0900

    H2데이터베스트를 이용하여 순수 JDBC와 스프링 통합 테스트 해보기

--- src/main/java/hellostart/startspring/SpringConfig.java
+++ src/main/java/hellostart/startspring/SpringConfig.java
@@ -1,13 +1,22 @@
 package hellostart.startspring;
 
+import hellostart.startspring.repository.JdbcMemberRepository;
 import hellostart.startspring.repository.MemberRepository;
-import hellostart.startspring.repository.MemoryMemberRepository;
 import hellostart.startspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
---
+++ src/test/java/hellostart/startspring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,54 @@
+package hellostart.startspring.service;
+
+import hellostart.startspring.domain.Member;
+import hellostart.startspring.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional// test에서 이용하면 각메소드 test를 할때마다 디비를 rollback해준다.// 따라서 테스트를 위해 넣어졌던 데이터가 없어진다.
+class MemberServiceIntegrationTest {
+
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given : 이것이 주어지면
+        Member member = new Member();
+        member.setName("spring");
+
+        // when : 이것을 실행했을때
+        Long saveId = memberService.join(member);
+
+        //then : 결과가 이게 나와야함
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        //then
+    }
+
+
+}
